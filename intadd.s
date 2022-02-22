b77f3b6bd9b9d29fa8a6f43866d729124398da5b
tjithan17 <49247011+tjithan17@users.noreply.github.com>
2022-02-21 22:34:17 -0800

    Update intadd.s
    Added possible add code

--- intadd.s
+++ intadd.s
@@ -4,4 +4,30 @@
     .global intadd
 
 intadd:
+    stp x29, x30, [sp,-16]!
+    add x29, sp, 0
+    str x19, [sp,-8]!
+    str x20, [sp,-8]!
 
+    mov x19, w2
+    mov x20, w3
+    
+    b while
+
+    ldr x19 ,[sp], 8
+    ldr x20 ,[sp], 8
+    ldp x29,x30, [sp], 16
+    
+    ret
+
+while:
+    mov x1, w2 //FIRST INTEGER
+    mov x2, w3 //SECOND INTEGER
+    
+    and x3, w2, w3//FIRST INTEGER, SECOND INTEGER
+    eorr x1, w2, w3//FIRST INTEGER, SECOND INTEGER
+
+    lsl x2, x3, #1
+    
+    cmp x2, #0
+    bne while
